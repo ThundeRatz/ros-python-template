1d33bb503356ec9eb2faec5f124d7f2c818c37c6
lucastrschneider <lucastrschneider@gmail.com>
2021-03-23 15:53:09 -0300

    📝 Add docstrings to distance sensors module

--- scripts/utils/distance_sensor.py
+++ scripts/utils/distance_sensor.py
@@ -3,22 +3,44 @@ from sensor_msgs.msg import Range
 
 class DistanceSensor:
     def __init__(self, topic_name):
+        """Cria um novo objeto para fazer a leitura do sensor de distância
+
+        Args:
+            topic_name (string): Nome do tópico do sensor de distância
+        """
         self.range = None
         self.max_range = None
         self.min_range = None
         self.topic_name = topic_name
 
     def initialise(self):
+        """Inicializa o subscriber para leitura do sensor
+        """
         rospy.Subscriber(self.topic_name, Range, self._callback)
-        rospy.loginfo(f"Node iniciado de distância do sensor {rospy.get_time()}")
- 
+        rospy.loginfo(f"Inicializando sensor de distância {rospy.get_time()}")
+
     def _callback(self, data):
+        """Função de callback necessária para inicializar o subscriber
+
+        Args:
+            data (sensor_msgs.msg.Range): Dados da leitura do sensor
+        """
         self.range = data.range
         self.max_range = data.max_range
         self.min_range = data.min_range
 
     def get_range(self):
+        """Método para obter o valor da última leitura de distância
+
+        Returns:
+            float: Distância em metros até o objeto mais próximo
+        """
         return self.range
-    
+
     def get_limits(self):
+        """Método para retornar o limite inferior e superior da leitura
+
+        Returns:
+            (float, float): Limite inferior e superior da leitura
+        """
         return (self.max_range,self.min_range)
