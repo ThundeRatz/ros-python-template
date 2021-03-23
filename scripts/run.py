47aae0867babfec55bc133b3d0c76f54cda03b23
lucastrschneider <lucastrschneider@gmail.com>
2021-03-23 15:43:43 -0300

    ⚡️ Update run script to stop when interrupted - Update loop rate to 60 Hz
    Co-authored-by: Felipe Gomes de Melo <felipegmelo.42@gmail.com>

--- scripts/run.py
+++ scripts/run.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python3
 
 import rospy
-from .control import setup, loop
+from std_msgs.msg import Float64
+
+from control import setup, loop
 
 
-CONTROL_RATE = 10  # Hz
+CONTROL_RATE = 60  # Hz
 
 
 def main():
-    rospy.init_node("robot_controller")
-    rospy.loginfo(f"Node iniciado de controle{rospy.get_time()}")
+    rospy.init_node("robot_controller", disable_signals=True)
+    rospy.loginfo(f"Node de controle iniciado {rospy.get_time()}")
     rate = rospy.Rate(CONTROL_RATE)
 
     setup()
@@ -24,3 +26,10 @@ if __name__ == "__main__":
         main()
     except rospy.ROSInterruptException:
         pass
+    except KeyboardInterrupt:
+        pass
+    finally:
+        left_motor_pub = rospy.Publisher('/modelo_carrinho/robot_left_controller/command', Float64, queue_size=1)
+        right_motor_pub = rospy.Publisher('/modelo_carrinho/robot_right_controller/command', Float64, queue_size=1)
+        left_motor_pub.publish(Float64(0))
+        right_motor_pub.publish(Float64(0))
